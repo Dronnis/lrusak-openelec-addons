132344fdbab3f20ebccdbe50232aff09997a1201
Lukas Rusak <lorusak@gmail.com>
2016-05-18 10:11:47 -0700

    repository.lrusak.openelec.addons: update for LibreELEC-8.0

--- addons/repository/repository.lrusak.openelec.addons/package.mk
+++ addons/repository/repository.lrusak.openelec.addons/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="repository.lrusak.openelec.addons"
-PKG_VERSION="7.0"
-PKG_REV="2"
+PKG_VERSION="8.0"
+PKG_REV="0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://addons.freestylephenoms.com"
